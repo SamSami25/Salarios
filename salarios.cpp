b5a32b029b225e03d8bf44b4f2dd8582a271fa40
Sami Aldaz <106607257+SamSami25@users.noreply.github.com>
2022-06-28 10:26:34 -0500

    Update salarios.cpp

--- salarios.cpp
+++ salarios.cpp
@@ -72,3 +72,129 @@ void Salarios::limpiar()
     ui->inNombre->setFocus(); 
 }
 
+void Salarios::calcular()
+{
+    // Obtener datos de la GUI
+    QString nombre = ui->inNombre->text();
+    int horas = ui->inHoras->value();
+    TipoJornada jornada;
+    if(ui->inMatutina->isChecked()){
+        jornada = TipoJornada::Matituna;
+    }else if (ui->inVespertina->isChecked()){
+        jornada = TipoJornada::Vespertina;
+    }else{
+        jornada = TipoJornada::Nocturna;
+    }
+    // Validacion de datos
+    if(nombre == "" || horas == 0){
+        QMessageBox::warning(this,"Advertencia","El Nombre y/o el Número de Horas es Incorrecto");
+        return;
+    }
+
+    // Establecer datos al controlador
+    m_contolador->setDatos(nombre, horas, jornada);
+    // Calcular salarios
+    if (m_contolador->calcular()) {
+        // Muestra los resultados
+        ui->outCalculos->appendPlainText(m_contolador->getDatos());
+    } else {
+        QMessageBox::critical(
+                    this,
+                    "Error",
+                    "No se puede Calcular el Salario.");
+    }
+    // Limpiar widgets
+    limpiar();
+    // Mostrar mensaje en la barra de estado
+    ui->statusbar->showMessage("Salario de " + nombre + " calculado.",5000);
+
+}
+
+
+void Salarios::on_actionCalcular_triggered()
+{
+    calcular();
+}
+
+
+void Salarios::on_actionSalir_triggered()
+{
+    if(ui->outCalculos->toPlainText().isEmpty()){
+        this->close();
+    }
+    QMessageBox::StandardButton reply = QMessageBox::question(this,"Salir","¿Desea guardar los cambios realizados?",QMessageBox::Save | QMessageBox::No | QMessageBox::Cancel);
+
+    if(reply == QMessageBox::Save){
+        on_actionGuardar_triggered();
+    }
+    if(reply == QMessageBox::Cancel){
+        return;
+    }
+    if(reply == QMessageBox::No){
+        QApplication::quit();
+    }
+}
+
+
+void Salarios::on_actionGuardar_triggered()
+{
+    // Abrir un cuadro de diálogo para seleccionar el path y archivo a guardar
+    QString nombreArchivo = QFileDialog::getSaveFileName(this,
+                                                   "Guardar Calculos de Salarios",
+                                                   QDir::home().absolutePath() + "/salarios.txt",
+                                                   "Archivos de texto (*.txt)");
+    // Crear un objeto File o un Archivo
+    QFile archivo(nombreArchivo);
+    // Tartar de abrir para escritura
+    if(archivo.open(QFile::WriteOnly | QFile::Truncate)){
+        // cRear un objeto 'stream' de texto
+        QTextStream salida(&archivo);
+        // Enviar los datos del resultado a la salida
+        salida << ui->outCalculos->toPlainText();
+        // Mostrar mensaje en la barra de estados
+        ui->statusbar->showMessage("Datos guardados en: " + nombreArchivo, 5000);
+        // Cerrar el archivo
+        archivo.close();
+    }else {
+        // Mensaje de error
+        QMessageBox::warning(this,
+                             "Guardar archivo",
+                             "No se puede acceder al archivo para guardar los datos.");
+    }
+
+
+}
+
+
+void Salarios::on_actionAcerca_de_triggered()
+{
+    // Crear un objeto del cuadro de diálogo
+    Acerca *dialog = new Acerca(this);
+    // Enviar datos a la otra ventana
+    dialog->setVersion(VERSION);
+    // Mostrar la venta en modo MODAL
+    dialog->exec();
+    // Luego de cerrar la ventana, se acceden a los datos de la misma
+    qDebug() << dialog->valor();
+
+}
+
+void Salarios::on_actionAbrir_triggered()
+{
+    QFile arch;
+    QTextStream io;
+    QString nombreArch;
+    nombreArch = QFileDialog::getOpenFileName(this, "Abrir");
+    arch.setFileName(nombreArch);
+    arch.open(QIODevice::ReadOnly | QIODevice::Text);
+    if(!arch.isOpen()){
+        QMessageBox::critical(this, "Error !!!", arch.errorString());
+        return;
+    }
+    io.setDevice(&arch);
+    ui->outCalculos->setPlainText(io.readAll());
+    arch.flush();
+    arch.close();
+}
+
+
